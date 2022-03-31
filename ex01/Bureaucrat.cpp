7eff103b42d3b87e3e7b59cadcc871b832d284b3
ywake <ywake@student.42tokyo.jp>
2022-03-31 17:18:17 +0900

    Add: ex01 init

---
+++ ex01/Bureaucrat.cpp
@@ -0,0 +1,82 @@
+#include "Bureaucrat.hpp"
+
+/*
+ * constructor destructor
+ */
+Bureaucrat::Bureaucrat() : _name("NoName"), _grade(LOWEST_GRADE) {}
+Bureaucrat::Bureaucrat(std::string const &name, int const grade) :
+    _name(name), _grade(grade)
+{
+  _checkGrade(_grade);
+}
+Bureaucrat::Bureaucrat(Bureaucrat const &copy) :
+    _name(copy.getName()), _grade(copy.getGrade())
+{
+  _checkGrade(_grade);
+}
+Bureaucrat::~Bureaucrat() {}
+
+/*
+ * operator
+ */
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &other)
+{
+  if (this != &other) {
+    this->_grade = other.getGrade();
+  }
+  return *this;
+}
+
+/*
+ * getter setter
+ */
+const std::string Bureaucrat::getName() const { return _name; }
+int Bureaucrat::getGrade() const { return _grade; }
+void Bureaucrat::_setGrade(int newGrade)
+{
+  _checkGrade(newGrade);
+  _grade = newGrade;
+}
+
+/*
+ * methods
+ */
+int Bureaucrat::gradeUp()
+{
+  _setGrade(_grade - 1);
+  return _grade;
+}
+int Bureaucrat::gradeDown()
+{
+  _setGrade(_grade + 1);
+  return _grade;
+}
+void Bureaucrat::_checkGrade(int grade) const
+{
+  if (grade < HIGHEST_GRADE) {
+    throw Bureaucrat::GradeTooHighException();
+  }
+  if (grade > LOWEST_GRADE) {
+    throw Bureaucrat::GradeTooLowException();
+  }
+}
+
+/*
+ * exception
+ */
+Bureaucrat::GradeTooHighException::GradeTooHighException(
+    const std::string &msg) :
+    std::out_of_range(msg)
+{
+}
+
+Bureaucrat::GradeTooLowException::GradeTooLowException(const std::string &msg) :
+    std::out_of_range(msg)
+{
+}
+
+std::ostream &operator<<(std::ostream &os, Bureaucrat const &b)
+{
+  os << b.getName() << ", bureaucrat grade " << b.getGrade() << ".";
+  return os;
+}
---
+++ ex01/Bureaucrat.hpp
@@ -0,0 +1,63 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <ostream>
+#include <stdexcept>
+#include <string>
+
+class Bureaucrat
+{
+private:
+  const std::string _name;
+  int _grade;
+  void _checkGrade(int grade) const;
+  void _setGrade(int newGrade);
+
+public:
+  static const int HIGHEST_GRADE = 1;
+  static const int LOWEST_GRADE = 150;
+
+  /*
+   * constructor destructor
+   */
+  Bureaucrat();
+  Bureaucrat(std::string const &name, int const grade);
+  Bureaucrat(Bureaucrat const &copy);
+  ~Bureaucrat();
+
+  /*
+   * operator
+   */
+  Bureaucrat &operator=(Bureaucrat const &other);
+
+  /*
+   * getter setter
+   */
+  const std::string getName() const;
+  int getGrade() const;
+
+  /*
+   * methods
+   */
+  int gradeUp();
+  int gradeDown();
+
+  /*
+   * exception
+   */
+  class GradeTooHighException : public std::out_of_range
+  {
+  public:
+    GradeTooHighException(const std::string &msg = "The grade is too high.");
+  };
+
+  class GradeTooLowException : public std::out_of_range
+  {
+  public:
+    GradeTooLowException(const std::string &msg = "The grade is too low.");
+  };
+};
+
+std::ostream &operator<<(std::ostream &os, Bureaucrat const &bureaucrat);
+
+#endif
